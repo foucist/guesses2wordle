029a1ce7c1813df70d8a68299a305dae62c2a887
James Robey <james.robey@gmail.com>
2022-01-22 09:46:54 -0800

    new mix project

---
+++ lib/guesses2wordle.ex
@@ -0,0 +1,18 @@
+defmodule Guesses2wordle do
+  @moduledoc """
+  Documentation for `Guesses2wordle`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Guesses2wordle.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/guesses2wordle_test.exs
@@ -0,0 +1,8 @@
+defmodule Guesses2wordleTest do
+  use ExUnit.Case
+  doctest Guesses2wordle
+
+  test "greets the world" do
+    assert Guesses2wordle.hello() == :world
+  end
+end
